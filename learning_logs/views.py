8c55d75c6e79380e9ce0dc85674d5d810a4a908c
Declan Li <DeclanLi528@gmail.com>
2025-07-09 20:39:32 +0800

    昨天落了一个edit entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -10,22 +12,29 @@ def index(request):
     return render(request, 'learning_logs/index.xhtml')
 
 
+@login_required
 def topics(request):
     """Show all topics."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.xhtml', context)
 
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
-    topic = Topic.objects.get(id=topic_id)  # This is called query
+    topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by(
         "-date_added")  # Use shell to test first
     context = {"topic": topic, 'entries': entries}
     return render(request, 'learning_logs/topic.xhtml', context)
 
 
+@login_required
 def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
@@ -44,6 +53,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.xhtml', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -62,3 +72,20 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.xhtml', context)
+
+
+@login_required
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != "POST":
+        form = EntryForm(instance=entry)  # 这里的argument会产生一个有老data的form
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.xhtml', context)
