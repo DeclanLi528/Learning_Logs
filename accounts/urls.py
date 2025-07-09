1c1b0c4c02e022a92f08259580ceb3a9c5dc9a7d
Declan Li <DeclanLi528@gmail.com>
2025-07-09 15:23:37 +0800

    添加一个注册系统

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,14 @@
 """Define URL patterns for accounts."""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
     # Include default auth urls.
     path('', include('django.contrib.auth.urls')),  # Django内置了认证的URL
+    # Registration page.
+    path('register/', views.register, name='register')
 
 ]
